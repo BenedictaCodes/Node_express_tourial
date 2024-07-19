4f4b40daa895a685f684ded0ff7cb02494f48a7f
BenedictaCodes <erhunmwunseeloghosabenedicta@gmail.com>
2024-07-19 04:47:26 -0700

    node.js

---
+++ 02-express-tutorial/app.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const app = express()
+
+app.use(express.static('./methods-public'))
+app.use(express.urlencoded({extended: false}))
+app.use(express.json())
+
+// app.post('/login',(req, res) =>{
+//     res.send('Hello Words')
+// });
+
+app.post('/login', (req,res) =>{
+    const {name} = req.body
+
+    if(name){
+         return res.status(200).send(`Welcome ${name}`)
+}
+    res.status(404).send("Please enter a name")
+})
+app.listen(5000,()=> {
+    console.log('Express Tutorial')
+})
+
+
